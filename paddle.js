d1aa9f4b9a8e2ff0b09ee7bb44d84d2db4608cf5
OlivierBarreau <76454614+OlivierBarreau@users.noreply.github.com>
2021-07-03 18:48:42 +0200

    Create paddle.js

---
+++ paddle.js
@@ -0,0 +1,15 @@
+class Paddle{
+    constructor() {
+        this.width = 150;
+        this.height = 30;
+        this,position = {
+
+            x: 1,
+            y: gameHeight - this.height - 10,
+        }
+    }
+    draw(ctx)
+    {
+        
+    }
+}
