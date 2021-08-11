64ceeeb69b8b3a9f2adef043b5dd9bc32060ab4f
OlivierBarreau <76454614+OlivierBarreau@users.noreply.github.com>
2021-08-12 01:00:47 +0200

    change

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
-let canvas = document.getElementById("gameScreen");
-let ctx = camvas.getcontext('26');
+import Serpent from "./Serpent.js";
+//import Nourriture from "./Nourriture.js";
+//import Obstacle from "./Obstacle.js";
+import Game from "./Game.js";
+
+/*Canvas*/
+const canvas = document.getElementById('canvasAvecCollision');
+/* Contexte graphique*/
+const ctx = canvas.getContext("2d");
+
+let serpent = new Serpent(600,500,"carre");
+
+let game = new Game(canvas,ctx, serpent);
+game.gameStart();
