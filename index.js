eac14332f9fbf3f9bd831c7dfeed66a000a98105
davito <jdavedarley@gmail.com>
2021-08-11 06:14:26 -0400

    deplacer nourriture serpent
    deplacement d'un seul nourriture , dans n'importe quelle direction,dans le cadre avec collision ; Le deplacement de plusieurs nourritures sera ft probablement pendant la journee

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 import Nourriture from "./nourriture.js";
 
-var mesNourritures = {
-    img0 : "./images/img"+ 0 + ".png",
-    img1 : "./images/img"+ 1 + ".png",
-    img2 : "./images/img"+ 2 + ".png",
-    img3 : "./images/img"+ 3 + ".png",
-    img4 : "./images/img"+ 4 + ".png",
-    img5 : "./images/img"+ 5 + ".png",
-    img6 : "./images/img"+ 6 + ".png"
-  }
+var mesNourritures = [
+  "./images/img"+ 0 + ".png",
+  "./images/img"+ 1 + ".png",
+  "./images/img"+ 2 + ".png",
+  "./images/img"+ 3 + ".png",
+  "./images/img"+ 4 + ".png",
+  "./images/img"+ 5 + ".png",
+  "./images/img"+ 6 + ".png"
+]
 
 
 
@@ -39,7 +39,6 @@ function remplirCanvas(collisionOuPas){
   console.log("je suis rentre ici canvas");
   var canvas;
   var nbNourriture = 0;
-  var mesNourritures = [];
 
 
 
@@ -49,32 +48,33 @@ function remplirCanvas(collisionOuPas){
   }else{
     canvas = document.getElementById("sansColli");
   }
-  var layout = canvas.getContext("2d");
+  var layout = canvas.getContext("2d"); 
 
-  /*let nourriture0 = new Nourriture(mesNourritures.img0,10,20,20);
-  let nourriture1 = new Nourriture(mesNourritures.img1,10,20,20);
-  let nourriture2 = new Nourriture(mesNourritures.img2,10,20,20);
-  let nourriture3 = new Nourriture(mesNourritures.img3,10,20,20);
-  let nourriture4 = new Nourriture(mesNourritures.img4,10,20,20);
-  let nourriture5 = new Nourriture(mesNourritures.img5,10,20,20);
-  let nourriture6 = new Nourriture(mesNourritures.img6,10,20,20);*/
-
-  while(1){
+  while(nbNourriture < 1){ // pour pas avoir plus de 2 nourritures sur le canvas en meme temps
     // NB on a un canvas de 700*700
-    if(nbNourriture <= 1){ // pour le debut jveux pas avoir plus de 2 nourritures sur le canvas en meme temps
       nbNourriture ++;
-      var xDepart = Math.floor(Math.random() *8) * 100; 
+      
+      // test 
+      console.log("nb de nourriture live "+ nbNourriture);
+
+      var xDepart =  Math.floor(Math.random() *8) * 100; 
       var yDepart = Math.floor(Math.random() *6) * 100;
       var direction = Math.floor(Math.random() *8);
       var grosseur = (Math.floor(Math.random() *4) * 10) + 20; // grosseur entre 20 px et 50 px
-      var vitesse = Math.floor(Math.random() *8) * 10;  // ici c'est un peu arbitraire
-      var nourriture = Math.floor(Math.random() *7) + "";
+      var vitesse =  (Math.floor(Math.random() *8)+1) * 1000;  // ici c'est un peu arbitraire
+      var nourriture = Math.floor(Math.random() *7) ;
       var ptsDeVie = Math.floor(Math.random() *9) + 1;
 
-      let maNourriture = new Nourriture(mesNourritures.img+nourriture ,ptsDeVie,grosseur,grosseur,xDepart,yDepart,vitesse,direction);
-      mesNourritures.push(maNourriture);
+      let maNourriture = new Nourriture(mesNourritures[nourriture],ptsDeVie,grosseur,grosseur,xDepart,yDepart,vitesse,direction);
       maNourriture.deplacementNourriture(layout);
-    }
+
+      let id = null;
+      clearInterval(id);
+      // references:
+      //https://stackoverflow.com/questions/457826/pass-parameters-in-setinterval-function
+      
+      id = setInterval( function() { maNourriture.frame(layout,id) ;} ,vitesse); // appelle la fonction frame chaque 1 seconde
+    
 
     // comment savoir qu'une nourriture n'est plus sur le canvas:
    /* for(var i = 0; i<nbNourriture ; i++){
@@ -83,16 +83,6 @@ function remplirCanvas(collisionOuPas){
 
   }
 
-/*  loadImages(mesImages, function(images){
-    layout.drawImage(images.img0,0,0,40,40);
-    layout.drawImage(images.img1,100,200,40,40);
-    layout.drawImage(images.img2,400,200,40,40);
-    layout.drawImage(images.img3,50,100,40,40);
-    layout.drawImage(images.img4,300,200,40,40);
-    layout.drawImage(images.img5,600,600,40,40);
-    layout.drawImage(images.img6,700,700,40,40);
-
-})*/
 
 }
 
--- nourriture.js
+++ nourriture.js
@@ -19,90 +19,137 @@ constructor(image,ptsDeVie,height,width,posX,posY,vitesse,direction){
 
 
  deplacementNourriture(layout){
-
-  console.log("je suis bien entre ici");
-  let positionX = this.getX();
-  let positionY = this.getY();
-  let id = null;
-  clearInterval(id);
-  id = setInterval(frame,this.vitesse); // appelle la fonction frame chaque 1 seconde
-
-  function frame(){
-    if (positionX <= 0 || positionX >= 700 || positionY <= 0 || positionY >= 700){
-      clearInterval(id); // arreter setInterval ; ca n'appelle plus frame
-    }else{
-      switch(this.direction){
-        case 0:
-          this.posX = this.posX - 1;
-          this.posY = this.posY - 1;
-          this.loadImages(this.image, function(images){
-            layout.drawImage(this.image,this.posX,this.posY,this.width,this.height);
-        })
-        break;
-
-        case 1:
-          this.posY = this.posY - 1;
-          this.loadImages(this.image, function(images){
-            layout.drawImage(this.image,this.posX,this.posY,this.width,this.height);
-        })
-        break;
-
-        case 2:
-          this.posX = this.posX + 1;
-          this.posY = this.posY - 1;
-          this.loadImages(this.image, function(images){
-            layout.drawImage(this.image,this.posX,this.posY,this.width,this.height);
-        })
-        break;
-
-        case 3:
-          this.posX = this.posX + 1;
-          this.loadImages(this.image, function(images){
-            layout.drawImage(this.image,this.posX,this.posY,this.width,this.height);
-        })
-        break;
-
-        case 4:
-          this.posX = this.posX + 1;
-          this.posY = this.posY + 1;
-          this.loadImages(this.image, function(images){
-            layout.drawImage(this.image,this.posX,this.posY,this.width,this.height);
-        })
-        break;
-
-        case 5:
-          this.posY = this.posY + 1;
-          this.loadImages(mesImages, function(images){
-            layout.drawImage(this.image,this.posX,this.posY,this.width,this.height);
-        })
-        break;
-
-        case 6:
-          this.posX = this.posX - 1;
-          this.posY = this.posY + 1;
-          this.loadImages(mesImages, function(images){
-            layout.drawImage(this.image,this.posX,this.posY,this.width,this.height);
-        })
-        break;
-
-        case 7:
-          this.posX = this.posX - 1;
-          this.loadImages(mesImages, function(images){
-            layout.drawImage(this.image,this.posX,this.posY,this.width,this.height);
-        })
-        break;
-      }
-
-
-    }
-
-
-
-
-  }
+  // pour test
+  console.log("-----------------------");
+  console.log("Ma nourriture se trouve en positionX :"+ this.posX + "positionY: "+ this.posY);
+  console.log("Ma nourriture va en direction :"+ this.direction);
+  console.log("Ma nourriture se deplace a vitesse de "+ this.vitesse);
+  console.log("-----------------------");
 
 }
 
+
+frame(layout,id){
+
+  let positionX = this.getX();
+  let positionY = this.getY();
+  let grosseur = this.getGrosseurNourriture();
+
+  if (positionX <= 0 || positionX >= 700 || positionY <= 0 || positionY >= 700){
+      clearInterval(id); // arreter setInterval ; ca n'appelle plus frame
+      // je dois enlever la nourriture du canvas!!
+  }else{
+    switch(this.direction){
+
+      case 0:
+        console.log("deplacement vers 0");
+        console.log("Le nouvel x est: "+this.posX);
+        console.log("Le nouvel y est: "+this.posY);
+
+        layout.clearRect(positionX,positionY,700,700); // pour enlever le "after Image" qd la nourriture se deplace
+        
+        var img1 = new Image();
+        // attendre pour que l'image soit load avant de le dessiner!!
+        img1.addEventListener('load', function(){
+          layout.drawImage(img1, positionX,positionY,40,40);
+        });
+        img1.src = this.image;
+        this.posX = this.posX - 1;
+        this.posY = this.posY - 1;
+      break;
+
+      case 1:
+        console.log("deplacement vers 1");
+        layout.clearRect(positionX,positionY,700,700); // pour enlever le "after Image" qd la nourriture se deplace
+        var img1 = new Image();
+        // attendre pour que l'image soit load avant de le dessiner!!
+        img1.addEventListener('load', function(){
+          layout.drawImage(img1, positionX,positionY,grosseur,grosseur);
+        });
+        img1.src = this.image;
+        this.posY = this.posY - 1;
+      break;
+
+      case 2:
+        console.log("deplacement vers 2");
+        layout.clearRect(positionX,positionY,700,700); // pour enlever le "after Image" qd la nourriture se deplace
+        var img1 = new Image();
+        // attendre pour que l'image soit load avant de le dessiner!!
+        img1.addEventListener('load', function(){
+          layout.drawImage(img1, positionX,positionY,grosseur,grosseur);
+        });
+        img1.src = this.image;
+        this.posX = this.posX + 1;
+        this.posY = this.posY - 1;
+      break;
+
+      case 3:
+        console.log("deplacement vers 3");
+        layout.clearRect(positionX,positionY,700,700); // pour enlever le "after Image" qd la nourriture se deplace
+        var img1 = new Image();
+        // attendre pour que l'image soit load avant de le dessiner!!
+        img1.addEventListener('load', function(){
+          layout.drawImage(img1, positionX,positionY,grosseur,grosseur);
+        });
+        img1.src = this.image;
+        this.posX = this.posX + 1;
+    
+      break;
+
+      case 4:
+        console.log("deplacement vers 4");
+        layout.clearRect(positionX,positionY,700,700); // pour enlever le "after Image" qd la nourriture se deplace
+        var img1 = new Image();
+        // attendre pour que l'image soit load avant de le dessiner!!
+        img1.addEventListener('load', function(){
+          layout.drawImage(img1, positionX,positionY,grosseur,grosseur);
+        });
+        img1.src = this.image;
+        this.posX = this.posX + 1;
+        this.posY = this.posY + 1;
+      break;
+
+      case 5:
+        console.log("deplacement vers 5");
+        layout.clearRect(positionX,positionY,700,700); // pour enlever le "after Image" qd la nourriture se deplace
+        var img1 = new Image();
+        // attendre pour que l'image soit load avant de le dessiner!!
+        img1.addEventListener('load', function(){
+          layout.drawImage(img1, positionX,positionY,grosseur,grosseur);
+        });
+        img1.src = this.image;
+        this.posY = this.posY + 1;
+      break;
+
+      case 6:
+        console.log("deplacement vers 6");
+        layout.clearRect(positionX,positionY,700,700); // pour enlever le "after Image" qd la nourriture se deplace
+        var img1 = new Image();
+        // attendre pour que l'image soit load avant de le dessiner!!
+        img1.addEventListener('load', function(){
+          layout.drawImage(img1, positionX,positionY,grosseur,grosseur);
+        });
+        img1.src = this.image;
+        this.posX = this.posX - 1;
+        this.posY = this.posY + 1;
+      break;
+
+      case 7:
+        console.log("deplacement vers 7");
+        layout.clearRect(positionX,positionY,700,700); // pour enlever le "after Image" qd la nourriture se deplace
+        var img1 = new Image();
+        // attendre pour que l'image soit load avant de le dessiner!!
+        img1.addEventListener('load', function(){
+          layout.drawImage(img1, positionX,positionY,grosseur,grosseur);
+        });
+        img1.src = this.image;
+        this.posX = this.posX - 1;
+      break;
+    }
+
+
+  }
+}
 /*
 Ces 2 fonctions permettent d'avoir la position actuelle d'une nourriture
 sur le canvas
@@ -113,26 +160,10 @@ getX(){
 getY(){
   return this.posY;
 }
-
-loadImages(sources, callback){
-  var images = {};
-  var loadedImages = 0;
-  var numImages = 0;
-
-  for(var src in sources){ // pr savoir le nb d'images qu'on a passe a cette fonction
-    numImages++;
-  }
-  for(var src in sources){
-    images[src] = new Image();
-    images[src].onload = function(){
-
-      if (++loadedImages >= numImages){
-        callback(images);
-      }
-    };
-    images[src].src = sources[src];
-  }
+getGrosseurNourriture(){
+  return this.height;
 }
+
 
 
 
